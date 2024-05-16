f3db5540936b1b1e6541a6eebadb62c3b20273db
Aayushadhika <nu6281806@gmail.com>
2024-05-16 20:59:15 +0545

    added splash screen

---
+++ lib/Screen/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+import 'login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const LoginScreen()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Splash Screen',
+              style: TextStyle(fontSize: 24.0),
+            ),
+            SizedBox(height: 20.0),
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
