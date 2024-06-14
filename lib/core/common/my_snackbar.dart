1d48450b68cf92c8ca0e1576cdbd42e4e9c25734
Aaysh <nu6281806@gmail.com>
2024-06-14 16:24:12 +0545

    added snackbar

---
+++ lib/core/common/my_snackbar.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:sagun/app/navigator_key/navigator_key.dart';
+
+
+showMySnackbar({
+  required String message,
+  Color? color,
+}) {
+  ScaffoldMessenger.of(AppNavigator.navigatorKey.currentState!.context)
+      .showSnackBar(SnackBar(
+    content: Text(message),
+    backgroundColor: color ?? Colors.red,
+    duration: const Duration(seconds: 2),
+    behavior: SnackBarBehavior.floating,
+  ));
+}
