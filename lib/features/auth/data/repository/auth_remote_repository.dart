e620709fb0c5f6f77e6e656110627dc440c20bef
Aaysh <nu6281806@gmail.com>
2024-06-21 11:04:34 +0545

    added api

---
+++ lib/features/auth/data/repository/auth_remote_repository.dart
@@ -0,0 +1,28 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:sagun/core/failure/failure.dart';
+
+import 'package:sagun/features/auth/data/data_source/remote/auth_remote_data_source.dart';
+import 'package:sagun/features/auth/domain/entity/auth_entity.dart';
+import 'package:sagun/features/auth/domain/repository/auth_repository.dart';
+
+final authRemoteRepositoryProvider = Provider<IAuthRepository>((ref) {
+  return AuthRemoteRepository(ref.read(authRemoteDataSourceProvider));
+});
+
+class AuthRemoteRepository implements IAuthRepository {
+  final AuthRemoteDataSource _authRemoteDataSource;
+
+  AuthRemoteRepository(this._authRemoteDataSource);
+
+  @override
+  Future<Either<Failure, bool>> registerUser(AuthEntity user) {
+    return _authRemoteDataSource.registerUser(user);
+  }
+
+  @override
+  Future<Either<Failure, bool>> loginUser(String email, String password) {
+    return _authRemoteDataSource.loginUser(email, password);
+  }
+}
