d420ec4c243010797f55eb53894727bf6d178d77
Aaysh <nu6281806@gmail.com>
2024-06-21 11:04:38 +0545

    added api

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -2,16 +2,16 @@ import 'dart:io';
 
 import 'package:dartz/dartz.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:sagun/core/error/failure.dart';
-import 'package:sagun/features/auth/data/repository/auth_local_repository.dart';
+
+import 'package:sagun/core/failure/failure.dart';
+import 'package:sagun/features/auth/data/repository/auth_remote_repository.dart';
 import 'package:sagun/features/auth/domain/entity/auth_entity.dart';
 
 final authRepositoryProvider = Provider<IAuthRepository>((ref) {
-  return ref.read(authLocalRepositoryProvider);
+  return ref.read(authRemoteRepositoryProvider);
 });
 
 abstract class IAuthRepository {
-  Future<Either<Faliure, bool>> registerStudent(AuthEntity student);
-  Future<Either<Faliure, bool>> loginStudent(String username, String password);
-  Future<Either<Faliure, String>> uploadProfilePicture(File file);
+  Future<Either<Failure, bool>> registerUser(AuthEntity user);
+  Future<Either<Failure, bool>> loginUser(String email, String password);
 }
